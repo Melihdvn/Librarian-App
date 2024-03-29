f2b32e3c87c850e92302e18d22c6b117849ceff1
Melihdvn <melihdvn@gmail.com>
2024-03-29 18:55:18 +0300

    Project upload

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Librarian-App
+# Librarian App
+
+## Overview
+
+The Librarian App is a command-line application designed to manage books, categories, borrow history, and more. It provides functionalities to add, edit, delete, list, search, borrow, and return books, as well as manage book categories.
+
+## Usage
+
+Default password is : 1234
+
+Upon launching the application, you will be greeted with the main menu:
+
+Welcome to the Librarian app! What would you like to do?
+
+You can navigate through the menu options using arrow keys and select an option by pressing Enter.
+
+The available options are:
+
+- **Book Options**: Perform various operations related to books.
+- **Category Options**: Manage book categories.
+- **Borrow History**: View borrowing history.
+- **About**: Information about the application and author.
+- **Options**: Additional settings and configurations.
+- **Exit**: Quit the application.
+
+## Installation
+
+- Clone this repository to your local machine.
+- Compile the code using your preferred IDE or compiler.
+- Run the compiled executable file.
+
+## Requirements
+
+- .NET Framework (or .NET Core) installed on your machine.
+
+## Author
+
+- [Melih Divan](https://www.linkedin.com/in/melihdivan/)
+
+## Support
+
+If you encounter any issues or have suggestions for improvements, please open an issue on the [project's GitHub repository](https://github.com/Melihdvn/Librarian-App).
---
+++ librarian-app-dll/LibraryManager.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using static System.Console;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+
+namespace LibraryManagement
+{
+    public static class LibraryManager
+    {
+        public static bool IsNumeric(object Expression)
+        {
+            double retNum;
+
+            bool isNum = Double.TryParse(Convert.ToString(Expression), System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
+            return isNum;
+        }
+
+        public static void InsertBook(Book book)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string filename = Path.Combine(path, "library.dat");
+
+            byte[] bookBytes = Book.BookToByteArrayBlock(book);
+            FileUtility.AppendBlock(bookBytes, filename);
+        }
+
+        public static void InsertCategory(Category category)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string filename = Path.Combine(path, "category.dat");
+
+            byte[] catBytes = Category.CategoryToByteArrayBlock(category);
+            FileUtility.AppendBlock(catBytes, filename);
+        }
+
+        public static void UpdateBook(Book book, int booknumber)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string filename = Path.Combine(path, "library.dat");
+
+            byte[] bookBytes = Book.BookToByteArrayBlock(book);
+            FileUtility.UpdateBlock(bookBytes, booknumber, Book.BOOK_DATA_BLOCK_SIZE, filename);
+        }
+
+        public static void UpdateCategory(Category category, int catnumber)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string filename = Path.Combine(path, "category.dat");
+
+            byte[] catBytes = Category.CategoryToByteArrayBlock(category);
+            FileUtility.UpdateBlock(catBytes, catnumber, Category.CATEGORY_MAX_LENGTH, filename);
+        }
+
+        public static void DeleteBook(int booknumber)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string filename = Path.Combine(path, "library.dat");
+
+            using (StreamReader sr = new StreamReader(File.Open("library.dat", FileMode.Open)))
+            {
+                string datalength = sr.ReadLine();
+                sr.Close();
+                FileUtility.DeleteBlock(booknumber, Book.BOOK_DATA_BLOCK_SIZE, filename);
+                do
+                {
+                    byte[] nextbookbytes = FileUtility.ReadBlock(booknumber + 1, Book.BOOK_DATA_BLOCK_SIZE, filename);
+                    FileUtility.UpdateBlock(nextbookbytes, booknumber, Book.BOOK_DATA_BLOCK_SIZE, filename);
+                    booknumber++;
+                } while (booknumber <= (((datalength.Length) / (Book.BOOK_DATA_BLOCK_SIZE)) - 1));
+
+                FileUtility.DeleteBlock(((datalength.Length) / (Book.BOOK_DATA_BLOCK_SIZE)), Book.BOOK_DATA_BLOCK_SIZE, filename);
+            }
+        }
+
+        public static void DeleteCategory(int catnumber)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string filename = Path.Combine(path, "category.dat");
+
+            using (StreamReader sr = new StreamReader(File.Open("category.dat", FileMode.Open)))
+            {
+                string datalength = sr.ReadLine();
+                sr.Close();
+                FileUtility.DeleteBlock(catnumber, Category.CATEGORY_MAX_LENGTH, filename);
+                do
+                {
+                    byte[] nextbookbytes = FileUtility.ReadBlock(catnumber + 1, Category.CATEGORY_MAX_LENGTH, filename);
+                    FileUtility.UpdateBlock(nextbookbytes, catnumber, Category.CATEGORY_MAX_LENGTH, filename);
+                    catnumber++;
+                } while (catnumber <= (((datalength.Length) / (Category.CATEGORY_MAX_LENGTH)) - 1));
+
+                FileUtility.DeleteBlock(((datalength.Length) / (Category.CATEGORY_MAX_LENGTH)), Category.CATEGORY_MAX_LENGTH, filename);
+            }
+        }
+
+        public static Book ReadBook(int booknumber)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string filename = Path.Combine(path, "library.dat");
+
+            byte[] bookWrittenBytesforBorrow = FileUtility.ReadBlock(booknumber, Book.BOOK_DATA_BLOCK_SIZE, filename);
+            Book bookWrittenObject = Book.ByteArrayBlockToBook(bookWrittenBytesforBorrow);
+            return bookWrittenObject;
+        }
+
+        public static Category ReadCategory(int catnumber)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string filename = Path.Combine(path, "category.dat");
+
+
+
+            byte[] categoryWrittenBytes = FileUtility.ReadBlock(catnumber, Category.CATEGORY_MAX_LENGTH, filename);
+            Category categoryWrittenObject = Category.ByteArrayBlockToCategory(categoryWrittenBytes);
+            return categoryWrittenObject;
+        }
+    }
+}
